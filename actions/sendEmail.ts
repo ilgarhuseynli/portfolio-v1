9644f1f8b8e1bd795a52e97824d705a31ac94e01
ilgarhuseynli <huseynli.dev@gmail.com>
2024-12-09 14:31:44 +0400

    resend contact form fixed

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "contact@ilgarhuseynli.com",
       to: "ilqarr.huseynli@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
