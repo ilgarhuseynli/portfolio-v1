7742f3f64926d7d06ef9f496b253ad5ca5d6a8de
Ilgar Huseynli <huseynli.dev@gmail.com>
2025-01-19 22:17:25 +0400

    shadcn and firebase integration

--- firebase.js
+++ firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA40nn2ATbTIlxYMOaeabtLyWzC2-oOkys",
-  authDomain: "ilgar-huseynli.firebaseapp.com",
-  projectId: "ilgar-huseynli",
-  storageBucket: "ilgar-huseynli.firebasestorage.app",
-  messagingSenderId: "620368752624",
-  appId: "1:620368752624:web:b577e312af11481e1b3735"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
